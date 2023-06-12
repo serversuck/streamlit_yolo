99535277ab5c39985c1d89cbe39ab2ffdaf3af32
ohm08164 <97542250+ohm08164@users.noreply.github.com>
2023-06-12 13:53:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.title("🌊 Under the sea detection")
 st.write("Upload your Image...")
 
 #model = torch.hub.load('./yolov5', 'custom', path='./last.pt', source='local')
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/last.pt', force_reload=True)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/b.pt', force_reload=True)
 
 uploaded_file = st.file_uploader("Choose .jpg pic ...", type="jpg")
 if uploaded_file is not None:
